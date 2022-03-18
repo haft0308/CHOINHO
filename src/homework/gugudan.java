c90ba26aa18ddea621e1af7b8093c90c4372c8b0
chochochoinho <bluesky76@naver.com>
2022-03-18 17:21:28 +0900

    first

---
+++ src/homework/gugudan.java
@@ -0,0 +1,35 @@
+package homework;
+
+import java.util.Scanner;
+
+public class gugudan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int a=0; 
+		int b=1;
+		
+		for (a=3; a<=3; a++ ) {
+			
+			for (b=1; b<=9; b++) {
+				System.out.printf("%d * %d = % d\n " ,a,b ,(a*b));
+				
+				
+				
+		Scanner sc= new Scanner(System.in);
+		int x = sc.nextInt();
+		
+		
+		
+				
+				
+			}
+		}
+
+		
+	}
+	
+}
+
